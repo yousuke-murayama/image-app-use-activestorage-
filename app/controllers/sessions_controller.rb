0ef26f8625d373c81151912ab044f577b4f661f8
Ubuntu <ubuntu@ip-172-31-22-170.ap-northeast-1.compute.internal>
2021-12-11 03:20:34 +0000

    modify-post-show

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   
   def new
   end
-
+  
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
